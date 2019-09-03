810c40f38f230186a6f41cbfaadb72a4a83b5a16
Robert Flanagan <robertflanagan95@gmail.com>
2019-09-03 13:42:02 -0400

    frontend form for adding a job

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="job-form section">
+        <div className="table-header">Add a Job</div>
+        <form>
+          <div>
+            <label for="job-num">Job Number: </label>
+            <input type="text" id="job-num" name="job_number"></input>
+          </div>
+          <div>
+            <label for="fixture-req">Fixture Required? </label>
+            <input type="checkbox" id="fixture-req" name="fixture_requirement"></input>
+          </div>
+          <div>
+            <label for="drawing-req">Drawing Required? </label>
+            <input type="checkbox" id="drawing-req" name="drawing_requirement"></input>
+          </div>
+          <div>
+            <label for="job-notes">Notes: </label>
+            <input type="text" id="job-notes" name="job_notes"></input>
+          </div>
+          <div>
+            <button type="submit">Add Job</button>
+          </div>
+        </form>
+      </div>
       <div className="models section">
         <div className="table-header">CAD Models</div>
         <table>
